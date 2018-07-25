cbe3e883eab0c957c30a6e578550d53942209c44
Ahmed kamil jemil <akjemil@gmail.com>
2018-07-25 08:56:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 const currentUser ='ahmed';
-const welcomeMessage=`Welcome to Flatbook, ${currentUser}`;
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
